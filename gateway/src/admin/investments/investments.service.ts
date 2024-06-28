9ff269753a07bc54037759b9bb647db670c7ce06
sakirhub <yazilimcisakir@gmail.com>
2024-06-28 21:07:26 +0300

    first commit 13-8

--- gateway/src/admin/investments/investments.service.ts
+++ gateway/src/admin/investments/investments.service.ts
@@ -216,7 +216,7 @@ export class InvestmentsService {
     ) {
       const callBackData = {
         transaction_id: investmentData.transaction_id,
-        user_id: investmentData.investor.site_user_id,
+        user_id: investmentData.investor.organization_user_id,
         status: 'approved',
         amount: amount,
       };
